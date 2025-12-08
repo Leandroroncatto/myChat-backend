6ce02dbae3aef2a5c7b74ed49fc018dad7429e27
Leandro <leandroroncatto18@gmail.com>
2025-12-08 17:11:25 -0300

    feat: create user model

--- src/main/java/com/example/myChat/Model/User.java
+++ src/main/java/com/example/myChat/Model/User.java
@@ -1,4 +1,71 @@
 package com.example.myChat.Model;
 
-public class User {
+import com.example.myChat.Model.Enum.UserStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String profilePictureUrl;
+
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 20)
+    private String displayName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(length = 150)
+    private String bio;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status = UserStatus.OFFLINE;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private Date created_At;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
 }
