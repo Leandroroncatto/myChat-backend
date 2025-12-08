19cd547ab98b38415e4520303d869b7d864a5e65
Leandro <leandroroncatto18@gmail.com>
2025-12-08 15:16:09 -0300

    feat: create argon2 bean

--- src/main/java/com/example/myChat/Configuration/Argon2Config.java
+++ src/main/java/com/example/myChat/Configuration/Argon2Config.java
@@ -1,5 +1,14 @@
 package com.example.myChat.Configuration;
 
-public class Argon2Config {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+public class Argon2Config {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder(16, 32, 1, 65536, 3);
+    }
 }
