be5d2185dc379320e2440a71e1d8efa0dab1a303
Leandro <leandroroncatto18@gmail.com>
2025-12-08 18:10:09 -0300

    feat: create login request dto

---
+++ src/main/java/com/example/myChat/Dtos/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.example.myChat.Dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    private String username;
+    private String email;
+    private String password;
+}
