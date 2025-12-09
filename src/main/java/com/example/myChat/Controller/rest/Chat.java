d7284ccf0cc3090ea62f7aa717a55950f0537398
Leandro <leandroroncatto18@gmail.com>
2025-12-09 17:27:57 -0300

    debug: testing jwt routes

---
+++ src/main/java/com/example/myChat/Controller/rest/Chat.java
@@ -0,0 +1,22 @@
+package com.example.myChat.Controller.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class Chat {
+
+    @GetMapping("/public")
+    public ResponseEntity<String> publicTest() {
+        return ResponseEntity.ok("A Rota Pública está Funcionando! (Não precisa de JWT)");
+    }
+
+    @GetMapping("/secure")
+    public ResponseEntity<String> secureTest() {
+        return ResponseEntity.status(HttpStatus.OK).body("A Rota Segura Funcionou! (JWT Válido)");
+    }
+}
