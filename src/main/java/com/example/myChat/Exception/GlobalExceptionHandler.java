1c45d3976b2f076f2efc64f1e235b9d0d2f6c814
Leandro <leandroroncatto18@gmail.com>
2025-12-09 20:37:21 -0300

    feat: create GlobalExceptionHandler

--- src/main/java/com/example/myChat/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/myChat/Exception/GlobalExceptionHandler.java
@@ -1,5 +1,67 @@
 package com.example.myChat.Exception;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler(Unauthorized.class)
+    public ResponseEntity<ErrorResponse> unauthorized(Unauthorized ex, WebRequest request) {
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        final HttpStatus status = HttpStatus.UNAUTHORIZED;
+        ErrorResponse errorResponse = new ErrorResponse(
+                status,
+                ex.getMessage()
+        );
+        Map<String, Object> details = new HashMap<>();
+        details.put("code", String.valueOf(status.value()));
+        details.put("path", path);
+        errorResponse.setDetails(details);
+
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(UserNotFound.class)
+    public ResponseEntity<ErrorResponse> userNotFound(UserNotFound ex, WebRequest request) {
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        final HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse errorResponse = new ErrorResponse(
+                status,
+                ex.getMessage()
+        );
+        Map<String, Object> details = new HashMap<>();
+        details.put("code", String.valueOf(status.value()));
+        details.put("path", path);
+        errorResponse.setDetails(details);
+
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    @ExceptionHandler(BadRequest.class)
+    public ResponseEntity<ErrorResponse> badRequest(BadRequest ex, WebRequest request) {
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        final HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErrorResponse errorResponse = new ErrorResponse(
+                status,
+                ex.getMessage()
+        );
+        Map<String, Object> details = new HashMap<>();
+        details.put("code", String.valueOf(status.value()));
+        details.put("path", path);
+        details.put("fields", ex.getFields());
+        errorResponse.setDetails(details);
+
+        return new ResponseEntity<>(errorResponse, status);
+    }
 }
