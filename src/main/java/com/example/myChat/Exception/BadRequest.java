ebbd253baa31b901e8e60069ff7a7501d71bdbd8
Leandro <leandroroncatto18@gmail.com>
2025-12-09 20:37:38 -0300

    feat: create BadRequest error class

---
+++ src/main/java/com/example/myChat/Exception/BadRequest.java
@@ -0,0 +1,21 @@
+package com.example.myChat.Exception;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class BadRequest extends RuntimeException {
+
+    Map<String, String> fields = new HashMap<>();
+
+    public BadRequest(String message) {
+        super(message);
+    }
+
+    public BadRequest(String message, Map<String, String> fields) {
+        super(message);
+        this.fields = fields;
+    }
+}
