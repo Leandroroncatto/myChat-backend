0a86b566f5fd9ab7b572b2b93281fc082f02e5a6
Leandro <leandroroncatto18@gmail.com>
2025-12-09 17:27:29 -0300

    feat: create message table model

--- src/main/java/com/example/myChat/Model/Message.java
+++ src/main/java/com/example/myChat/Model/Message.java
@@ -1,4 +1,41 @@
 package com.example.myChat.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "messages")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User sendBy;
+
+    @ManyToOne
+    @JoinColumn(name = "chatRoomId", nullable = false)
+    private ChatRoom chatRoom;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private Date timestamp;
 }
