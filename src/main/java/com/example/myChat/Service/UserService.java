feb7056dd5d44416583f5f86b68c78f404b924f6
Leandro <leandroroncatto18@gmail.com>
2025-12-09 20:44:02 -0300

    feat: create 2 new helper functions to simplify registerUser function

--- src/main/java/com/example/myChat/Service/UserService.java
+++ src/main/java/com/example/myChat/Service/UserService.java
@@ -1,14 +1,17 @@
 package com.example.myChat.Service;
 
-import com.example.myChat.Dtos.LoginRequest;
-import com.example.myChat.Dtos.RegisterRequest;
+import com.example.myChat.Dtos.request.LoginRequest;
+import com.example.myChat.Dtos.request.RegisterRequest;
+import com.example.myChat.Exception.BadRequest;
+import com.example.myChat.Exception.Unauthorized;
 import com.example.myChat.Model.User;
 import com.example.myChat.Repository.UserRepository;
-import org.hibernate.annotations.NotFound;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -24,21 +27,58 @@ public class UserService {
 
     public Optional<User> authenticateUser(LoginRequest request) {
         String identifier = request.getEmail() == null ? request.getUsername() : request.getEmail();
-        Optional<User> user = userRepository.findByUsernameOrEmail(identifier, identifier);
 
-        return user;
+        return Optional.ofNullable(userRepository.findByUsernameOrEmail(identifier, identifier)
+                .orElseThrow(() -> new Unauthorized("Invalid credentials")));
     }
 
-    public User registerUser(RegisterRequest request) {
+    public Optional<User> registerUser(RegisterRequest request) {
+        Map<String, String> errors = validateRequest(request);
+
+        if (!errors.isEmpty()) {
+            throw new BadRequest("Invalid form", errors);
+        }
+
+        User user = buildUser(request);
+
+        try {
+            User savedUser = userRepository.save(user);
+            return Optional.of(savedUser);
+        } catch (DataIntegrityViolationException e) {
+            throw handleDatabaseError(e);
+        }
+    }
+
+    private Map<String, String> validateRequest(RegisterRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        if (request.getUsername() == null || request.getUsername().isBlank()) {
+            errors.put("username", "Required field");
+        }
+        if (request.getEmail() == null || request.getEmail().isBlank()) {
+            errors.put("email", "Required field");
+        }
+        if (request.getDisplayName() == null || request.getDisplayName().isBlank()) {
+            errors.put("email", "Required field");
+        }
+        if (request.getPassword() == null || request.getPassword().isBlank()) {
+            errors.put("password", "Required field");
+        }
+        return errors;
+    }
+
+    private User buildUser(RegisterRequest request) {
         User user = new User();
         user.setUsername(request.getUsername());
         user.setDisplayName(request.getDisplayName());
         user.setEmail(request.getEmail());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
-
-        userRepository.save(user);
-
         return user;
     }
 
+    private BadRequest handleDatabaseError(DataIntegrityViolationException e) {
+        Map<String, String> errors = new HashMap<>();
+        System.out.println(e.getMessage());
+
+        return new BadRequest(e.getMessage());
+    }
 }
