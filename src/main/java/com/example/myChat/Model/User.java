5c5df50812565d0890573e29918b2acf214c8473
Leandro <leandroroncatto18@gmail.com>
2025-12-09 17:27:14 -0300

    refactor: made email & username unique

--- src/main/java/com/example/myChat/Model/User.java
+++ src/main/java/com/example/myChat/Model/User.java
@@ -32,13 +32,13 @@ public class User implements UserDetails {
 
     private String profilePictureUrl;
 
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = 20, unique = true)
     private String username;
 
     @Column(nullable = false, length = 20)
     private String displayName;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
