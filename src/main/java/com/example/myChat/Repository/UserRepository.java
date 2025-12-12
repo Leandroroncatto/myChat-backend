36e6a7183b98a23a821eeed17db3c5b375e9c8ec
Leandro <leandroroncatto18@gmail.com>
2025-12-11 21:09:31 -0300

    feat: add findByEmail query

--- src/main/java/com/example/myChat/Repository/UserRepository.java
+++ src/main/java/com/example/myChat/Repository/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsernameOrEmail(String name, String email);
     Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
