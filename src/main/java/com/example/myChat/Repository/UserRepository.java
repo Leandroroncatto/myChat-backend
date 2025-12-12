a0aa0c8aa15ddc823254a892010e5a0e7d2753b5
Leandro <leandroroncatto18@gmail.com>
2025-12-12 16:56:22 -0300

    feat: add user search sql query

--- src/main/java/com/example/myChat/Repository/UserRepository.java
+++ src/main/java/com/example/myChat/Repository/UserRepository.java
@@ -3,11 +3,16 @@ package com.example.myChat.Repository;
 import com.example.myChat.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsernameOrEmail(String name, String email);
+
     Optional<User> findByUsername(String username);
+
     Optional<User> findByEmail(String email);
+
+    List<User> findByUsernameContainingIgnoreCase(String username);
 }
