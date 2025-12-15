e61fca85483d7a9d8d72eafcf57fd3aad8ca84b5
Leandro <leandroroncatto18@gmail.com>
2025-12-15 15:40:31 -0300

    feat: create /me route & mountUserResponse method

--- src/main/java/com/example/myChat/Controller/rest/UserController.java
+++ src/main/java/com/example/myChat/Controller/rest/UserController.java
@@ -1,14 +1,17 @@
 package com.example.myChat.Controller.rest;
 
+import com.example.myChat.Dtos.response.UserResponseDto;
 import com.example.myChat.Model.User;
 import com.example.myChat.Service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,16 +28,24 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(@RequestParam(value = "search", required = false, defaultValue = "") String search) {
         List<User> users = userService.getAllUsers(search);
-
         return ResponseEntity.ok().body(users);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable UUID id) {
+    public ResponseEntity<UserResponseDto> getUser(@PathVariable UUID id) {
         User user = userService.getUser(id);
-        return ResponseEntity.ok().body(user);
+        UserResponseDto response = mountUserResponse(user);
+
+        return ResponseEntity.ok().body(response);
     }
 
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDto> getLoggedUserInfo() {
+        User user = userService.getLoggedInUserInfo();
+        UserResponseDto response = mountUserResponse(user);
+
+        return ResponseEntity.ok().body(response);
+    }
 
     /*
 
@@ -48,4 +59,15 @@ public class UserController {
 
      */
 
+    private UserResponseDto mountUserResponse(User user) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setId(user.getId());
+        userResponseDto.setBio(user.getBio());
+        userResponseDto.setProfilePictureUrl(user.getProfilePictureUrl());
+        userResponseDto.setEmail(user.getEmail());
+        userResponseDto.setUsername(user.getUsername());
+        userResponseDto.setDisplayName(user.getDisplayName());
+
+        return userResponseDto;
+    }
 }
