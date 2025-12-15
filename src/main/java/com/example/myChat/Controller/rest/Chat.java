677ccde1a1cb4eb80cd9de27bcd60376084ea9e3
Leandro <leandroroncatto18@gmail.com>
2025-12-15 15:00:27 -0300

    Update Chat.java

--- src/main/java/com/example/myChat/Controller/rest/Chat.java
+++ src/main/java/com/example/myChat/Controller/rest/Chat.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/api/v1/test")
 public class Chat {
 
     @GetMapping("/public")
