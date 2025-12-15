385dba69f45852ebc23295a4a1dca48adf75b037
Leandro <leandroroncatto18@gmail.com>
2025-12-15 19:27:36 -0300

    feat: add update user route

--- src/main/java/com/example/myChat/Controller/rest/UserController.java
+++ src/main/java/com/example/myChat/Controller/rest/UserController.java
@@ -1,14 +1,13 @@
 package com.example.myChat.Controller.rest;
 
+import com.example.myChat.Dtos.request.UpdateUserRequestDto;
 import com.example.myChat.Dtos.response.UserResponseDto;
 import com.example.myChat.Model.User;
 import com.example.myChat.Service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -46,6 +45,12 @@ public class UserController {
         return ResponseEntity.ok().body(response);
     }
 
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserResponseDto> updateUser(@PathVariable UUID id, @RequestBody UpdateUserRequestDto updates) {
+        User user = userService.updateUser(id, updates);
+        UserResponseDto response = mountUserResponse(user);
+        return ResponseEntity.ok().body(response);
+    }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
@@ -53,13 +58,6 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
-    /*
-    public ResponseEntity<User> updateUser() {
-
-    }
-
-
-     */
 
     private UserResponseDto mountUserResponse(User user) {
         UserResponseDto userResponseDto = new UserResponseDto();
