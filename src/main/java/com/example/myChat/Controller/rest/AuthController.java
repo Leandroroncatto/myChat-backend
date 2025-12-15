f90d7491ec99f621af356ded056d7507e8d907cd
Leandro <leandroroncatto18@gmail.com>
2025-12-15 14:57:29 -0300

    feat: return token and expiration on login and register

--- src/main/java/com/example/myChat/Controller/rest/AuthController.java
+++ src/main/java/com/example/myChat/Controller/rest/AuthController.java
@@ -1,18 +1,20 @@
 package com.example.myChat.Controller.rest;
 
-import com.example.myChat.Dtos.request.LoginRequest;
-import com.example.myChat.Dtos.request.RegisterRequest;
+import com.example.myChat.Dtos.request.LoginRequestDto;
+import com.example.myChat.Dtos.request.RegisterRequestDto;
+import com.example.myChat.Dtos.response.AuthResponseDto;
+import com.example.myChat.Dtos.response.UserResponseDto;
 import com.example.myChat.Model.User;
 import com.example.myChat.Service.JwtService;
 import com.example.myChat.Service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
@@ -25,18 +27,28 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
+    public ResponseEntity<AuthResponseDto> register(@RequestBody RegisterRequestDto registerRequest) {
         User user = authService.registerUser(registerRequest);
-
         String token = jwtService.generateToken(user.getUsername());
-        return ResponseEntity.status(HttpStatus.CREATED).body("TOKEN: " + token);
+        AuthResponseDto authResponseDto = mountAuthResponseDto(token);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(authResponseDto);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<AuthResponseDto> login(@RequestBody LoginRequestDto loginRequest) {
         User user = authService.loginUser(loginRequest);
-
         String token = jwtService.generateToken(user.getUsername());
-        return ResponseEntity.status(HttpStatus.OK).body("TOKEN: " + token);
+        AuthResponseDto authResponseDto = mountAuthResponseDto(token);
+
+        return ResponseEntity.status(HttpStatus.OK).body(authResponseDto);
+    }
+
+    private AuthResponseDto mountAuthResponseDto(String token) {
+        AuthResponseDto authResponseDto = new AuthResponseDto();
+        authResponseDto.setToken(token);
+        authResponseDto.setExpiresAt(jwtService.extractExpirationInstant(token));
+
+        return authResponseDto;
     }
 }
