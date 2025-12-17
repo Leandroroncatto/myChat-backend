9f9a122709d168990facd4c58a2cafc9fc5272cb
Leandro <leandroroncatto18@gmail.com>
2025-12-17 19:47:46 -0300

    refactor: implement verify email logic & refactored responses

--- src/main/java/com/example/myChat/Service/AuthService.java
+++ src/main/java/com/example/myChat/Service/AuthService.java
@@ -12,6 +12,8 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.*;
 
 @Service
@@ -21,12 +23,15 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final LoginFormHelper loginFormHelper;
     private final UserFormHelper userFormHelper;
+    private final EmailSender emailSender;
 
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, LoginFormHelper loginFormHelper, UserFormHelper userFormHelper) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, LoginFormHelper loginFormHelper, UserFormHelper userFormHelper, EmailSender emailSender) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.loginFormHelper = loginFormHelper;
         this.userFormHelper = userFormHelper;
+        this.emailSender = emailSender;
+
     }
 
     public User loginUser(LoginRequestDto request) {
@@ -44,10 +49,14 @@ public class AuthService {
             throw new Unauthorized("Invalid Credentials");
         }
 
+        if (!user.isEnabled()) {
+            throw new Unauthorized("Account not activated. Please verify your email.");
+        }
+
         return user;
     }
 
-    public User registerUser(RegisterRequestDto request) {
+    public void registerUser(RegisterRequestDto request) {
         Map<String, String> errors = userFormHelper.validateRegister(request);
 
         if (!errors.isEmpty()) {
@@ -55,12 +64,28 @@ public class AuthService {
         }
 
         User user = buildUser(request);
-
         try {
-            return userRepository.save(user);
+            user.setVerificationExpiresIn(Instant.now().plus(Duration.ofMinutes(20)));
+            userRepository.save(user);
+            emailSender.sendVerificationEmail(user.getEmail(), user.getVerificationToken());
         } catch (DataIntegrityViolationException e) {
             throw new BadRequest("Something went wrong!");
         }
+    }
+
+    public void verifyUser(String token) {
+        User user = userRepository.findByVerificationToken(token)
+                .orElseThrow(() -> new BadRequest("Invalid or expired verification link."));
+
+        if (user.getVerificationExpiresIn().isBefore(Instant.now())) {
+            throw new BadRequest("Verification link expired");
+        }
+
+        user.setEnabled(true);
+        user.setVerificationToken(null);
+        user.setVerificationExpiresIn(null);
+
+        userRepository.save(user);
     }
 
     private User buildUser(RegisterRequestDto request) {
@@ -69,7 +94,13 @@ public class AuthService {
         user.setDisplayName(request.getDisplayName());
         user.setEmail(request.getEmail());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setEnabled(false);
+        user.setVerificationToken(generateVerificationToken());
         return user;
+    }
+
+    private String generateVerificationToken() {
+        return UUID.randomUUID().toString();
     }
 }
 
