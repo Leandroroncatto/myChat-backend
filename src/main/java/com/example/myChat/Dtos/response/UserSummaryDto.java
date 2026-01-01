a2221a881d88a4a68141d495ccc809fee1e16069
Leandro <leandroroncatto18@gmail.com>
2026-01-01 08:49:37 -0300

    feat: create UserSummaryDto

---
+++ src/main/java/com/example/myChat/Dtos/response/UserSummaryDto.java
@@ -0,0 +1,9 @@
+package com.example.myChat.Dtos.response;
+
+import java.util.UUID;
+
+public class UserSummaryDto {
+    private UUID userId;
+    private String profilePicture;
+    private String username;
+}
