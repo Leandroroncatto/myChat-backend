f1160c26b17607e7dae4266f46c36bc6b090ea03
Leandro <leandroroncatto18@gmail.com>
2026-01-01 08:50:14 -0300

    placeholder

---
+++ src/main/java/com/example/myChat/Service/ChatService.java
@@ -0,0 +1,16 @@
+package com.example.myChat.Service;
+
+import com.example.myChat.Dtos.response.ChatRoomDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ChatService {
+
+    public void resolveChat(UUID id, String roomId) {
+    }
+
+    public ChatRoomDto loadChatRoomDetails(UUID userId, String roomId) {
+    }
+}
