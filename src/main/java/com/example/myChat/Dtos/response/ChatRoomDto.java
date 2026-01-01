61a5038a6b3c186511b6ff93972f8319ae61b6ed
Leandro <leandroroncatto18@gmail.com>
2026-01-01 08:49:00 -0300

    feat: create ChatRoomDto

---
+++ src/main/java/com/example/myChat/Dtos/response/ChatRoomDto.java
@@ -0,0 +1,17 @@
+package com.example.myChat.Dtos.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+public class ChatRoomDto {
+    private String roomId;
+    private List<MessageDto> messages;
+    private List<UserSummaryDTO> participants;
+    private String status;
+    private LocalDateTime createdAt;
+}
